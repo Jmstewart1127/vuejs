f35aac9b2ccf811980dae5fa5a628e730ebf43dd
Jmstewart1127 <Jmstewart1127@gmail.com>
2018-04-17 19:06:28 -0500

    made nav and a test component

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand">Vue Router Example</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <router-link to="/">Dashboard</router-link>
+            <router-link to="/test">Dashboard</router-link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3>YO This is a test cuh</h3>
+</template>
+
+<script>
+export default {
+  name: 'Test'
+}
+</script>
+
+<style scoped>
+  h3 {
+    color: dodgerblue;
+  }
+</style>
